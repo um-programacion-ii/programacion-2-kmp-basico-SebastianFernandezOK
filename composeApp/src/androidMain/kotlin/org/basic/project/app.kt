44c04b8bff68446d95aea71ba732df687115982a
SebastianFernandezOK <sebastianfernandez2812@gmail.com>
2025-08-26 18:24:04 -0300

    push clase 26-8

--- composeApp/src/androidMain/kotlin/org/basic/project/app.kt
+++ composeApp/src/androidMain/kotlin/org/basic/project/app.kt
@@ -1,28 +1,59 @@
 package org.basic.project
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.Navigator
 
 @Composable
 fun App() {
     MaterialTheme {
+        Navigator(MainScreen()) // 👈 Voyager arranca con MainScreen
+    }
+}
+
+class MainScreen : Screen {
+    @Composable
+    override fun Content() {
+        var name by remember { mutableStateOf("") }
+
         Column(
             modifier = Modifier
-                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .background(MaterialTheme.colorScheme.primaryContainer)
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Hola mundo!!!")
-            Text(text = "Programacion 2")
-            Text(text = "Android Studio")
+            // Campo de texto numérico
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Ingrese un número") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            // Texto animado solo si hay input
+            AnimatedVisibility(visible = name.isNotEmpty()) {
+                Text(
+                    text = "Versión animada $name",
+                    fontSize = 24.sp
+                )
+            }
         }
     }
 }
